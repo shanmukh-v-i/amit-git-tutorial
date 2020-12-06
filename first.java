4fe4330f6650fc9f26dac5045a44ef5d38b91431
Amit <shanmukh.v.i@gmail.com>
2020-12-07 00:40:06 +0530

    new java example

--- first.java
+++ first.java
@@ -1,4 +1,5 @@
-class first{}[D
-[D[D[D[A[A[A[B[B[C[C[C[C[C[C[C[C[C[C[C[Cpublic static[2~[D[D[D[D[D[D[D[D[D[D[D[D[D
-[D[D[D[A[A[B[B[C[A[A[D[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[2~ void main()[DString h[]){System.out.println[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[s[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[()[1;5D""[Dhello Git");
+public class First{
+	public static void main(String h[]){
+		System.out.println("Hello Git");
+	}
 }
