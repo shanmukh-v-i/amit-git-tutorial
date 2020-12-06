d14618d226e59ea6cdb995dc32f8063bacb74a0b
Amit <shanmukh.v.i@gmail.com>
2020-12-07 00:17:44 +0530

    jaba example

---
+++ first.java
@@ -0,0 +1,4 @@
+class first{}[D
+[D[D[D[A[A[A[B[B[C[C[C[C[C[C[C[C[C[C[C[Cpublic static[2~[D[D[D[D[D[D[D[D[D[D[D[D[D
+[D[D[D[A[A[B[B[C[A[A[D[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[2~ void main()[DString h[]){System.out.println[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[s[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[()[1;5D""[Dhello Git");
+}
